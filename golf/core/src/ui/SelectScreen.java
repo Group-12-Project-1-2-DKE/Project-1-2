cbca4975e7b1da092883abf31ca466846b32a031
Casssietje <Casper@zonnelaan.be>
2020-03-19 11:39:27 +0100

    changes
    4.0

--- golf/core/src/ui/SelectScreen.java
+++ golf/core/src/ui/SelectScreen.java
@@ -9,18 +9,9 @@ package ui;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-import java.awt.Graphics;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.imageio.ImageIO;
-
-
 
 public class SelectScreen extends JFrame {
-    static SelectScreen frame = new SelectScreen("Golf - Menu");
+    static SelectScreen frame = new SelectScreen("Golf 2D - Menu");
     GridLayout experimentLayout = new GridLayout(1,2);
 
     public SelectScreen(String name) {
