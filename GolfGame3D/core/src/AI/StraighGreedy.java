1073b1cedf6056983acdb4f5492d7b9438451faa
Mc1cheeseburger <43991581+Mc1cheeseburger@users.noreply.github.com>
2020-05-12 20:25:31 +0200

    Yet another update to the AI

--- GolfGame3D/core/src/AI/StraighGreedy.java
+++ GolfGame3D/core/src/AI/StraighGreedy.java
@@ -30,23 +30,51 @@ public class StraighGreedy implements AI{
         double factor = step_size/direction.length();
         Vector2D scaled_direction = direction.multiply(factor);
         Vector2D Ffric = direction.multiply(-ball.getMass() * grav_constant * course.getFrictionCoefficient());
+        Vector2D Fresist = new Vector2D(0,0);
 
         Vector2D returnvec = new Vector2D(direction.getX(), direction.getY());
-        for (int i = 0; i < heights.length - 1; i++){
+        for (int i = 0; i < heights.length - 1; i++) {
             double h = heights[i];
-            double h2 = heights[i+1];
+            double h2 = heights[i + 1];
             double difference = h2 - h;
-            Vector2D gradient = new Vector2D(difference/scaled_direction.getX(), difference/scaled_direction.getY());
+            Vector2D gradient = new Vector2D(difference / scaled_direction.getX(), difference / scaled_direction.getY());
             Vector2D Fgrav = gradient.multiply(-ball.getMass() * grav_constant);
-            Vector2D Fresist = Ffric.add(Fgrav);
-            Vector2D acc = Fresist.multiply((1/ball.getMass())*(factor));
+            Fresist = Ffric.add(Fgrav);
+        }
+        Vector2D acc = Fresist.multiply((-1/ball.getMass())*(factor));
+        double accX = acc.getX();
+        double accY = acc.getY();
+        if(direction.getY()/direction.getX() > acc.getY()/acc.getX()){
+            acc.setX(acc.getY()*direction.getX()/direction.getY());
+            accX -= acc.getX();
+            accX *= 2;
+        }
+        else if(direction.getY()/direction.getX() < acc.getY()/acc.getX()){
+            acc.setY(acc.getX()*direction.getY()/direction.getX());
+            accY -= acc.getY();
+            accY *= 2;
+        }
+
+        returnvec = acc.multiply((2*direction.length()));
+        // returnvec = returnvec.sqrt();
+
+        if(accX == acc.getX()){
+            returnvec.setY(returnvec.getY()+acc.getY());
+        }
+        else if(accY == acc.getY()){
+            returnvec.setX(returnvec.getX()+acc.getX());
+        }
+
+        return returnvec;
+            /*
             if (acc.getX() * scaled_direction.getX() < 0){
                 returnvec.setX(returnvec.getX() - acc.getX());
             }
             if (acc.getY() * scaled_direction.getY() < 0){
                 returnvec.setY(returnvec.getY() - acc.getY());
             }
-        }
-        return returnvec;
+             */
+
+
     }
 }
