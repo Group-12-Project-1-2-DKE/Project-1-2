a08125f58f5f6514aee6a9a9e99471e4670f6724
ArthurVieillevoye <56159647+ArthurVieillevoye@users.noreply.github.com>
2020-06-16 19:13:39 +0200

    Nicer maze frame
    Nicer maze frame

--- GolfGame3D/core/src/com/mygdx/game/Menus/MazeOption.java
+++ GolfGame3D/core/src/com/mygdx/game/Menus/MazeOption.java
@@ -1,6 +1,5 @@
 package com.mygdx.game.Menus;
 
-import Course.Nodes.Variable;
 import Reader.FileReaders;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
@@ -26,17 +25,7 @@ public class MazeOption implements Screen {
     private Texture playButtonInactive;
     private Texture backButtonActivated;
     private Texture backButtonInactivated;
-
-    // The position and size of the pictures.
-    private final int PLAY_WIDTH = 300;
-    private final int PLAY_HEIGHT = 120;
-    private final int BACK_WIDTH = 125;
-    private final int BACK_HEIGHT = 125;
-    private final int PLAY_Y = 75;
-
-    // The textfields on which the user will enter its values.
-    private TextField mazeX;
-    private TextField mazeY;
+    private Texture mazePicture;
 
     public MazeOption(ScreenSpace game) {
         this.game = game;
@@ -64,17 +53,19 @@ public class MazeOption implements Screen {
         playButtonInactive = new Texture("play_button_inactive.png");
         backButtonActivated = new Texture("back_arrow_activated.png");
         backButtonInactivated = new Texture("back_arrow_inactivated.png");
+        mazePicture = new Texture("mazeLogo.png");
 
         // Create the skin of the textfields
         Skin customizedMenuSkin = new Skin(Gdx.files.internal("uiskin.json"));
         // Create the textFields and add them to the "frame".
-        mazeX = new TextField(String.valueOf(10), customizedMenuSkin);
+        // The textfields on which the user will enter its values.
+        TextField mazeX = new TextField(String.valueOf(10), customizedMenuSkin);
         mazeX.setPosition(25,675);
         mazeX.setSize(200, 30);
         mazeX.setColor(Color.WHITE);
         stage.addActor(mazeX);
 
-        mazeY = new TextField(String.valueOf(10), customizedMenuSkin);
+        TextField mazeY = new TextField(String.valueOf(10), customizedMenuSkin);
         mazeY.setPosition(25,525);
         mazeY.setSize(200, 30);
         mazeY.setColor(Color.WHITE);
@@ -97,13 +88,21 @@ public class MazeOption implements Screen {
         font.draw(game.batch,"Maze length x:", 25,750);
         font.draw(game.batch,"Maze length y:", 25,600);
 
+        int MAZE_X = 375;
+        int MAZE_WIDTH = 300;
+        int MAZE_HEIGHT = 300;
+        game.batch.draw(mazePicture, MAZE_X, 250 , MAZE_WIDTH, MAZE_HEIGHT);
+
         // Create the play button.
         int x = ScreenSpace.WIDTH / 2;
+        // The position and size of the pictures.
+        int PLAY_WIDTH = 300;
+        int PLAY_HEIGHT = 120;
+        int PLAY_Y = 75;
         if (Gdx.input.getX() < x + PLAY_WIDTH && Gdx.input.getX() > x && ScreenSpace.HEIGHT - Gdx.input.getY() < PLAY_Y + PLAY_HEIGHT && ScreenSpace.HEIGHT - Gdx.input.getY() > PLAY_Y){
             // If the cursor is around the button, draw the activated button.
             game.batch.draw(playButtonActive, x, PLAY_Y, PLAY_WIDTH, PLAY_HEIGHT);
             if (Gdx.input.isTouched()){
-                // TODO implement the creation of the maze.
                 this.dispose();
                 Variables.function = "1";
                 game.setScreen(new GolfGame(game));
@@ -116,9 +115,11 @@ public class MazeOption implements Screen {
 
         // Create the go back button.
         int z = 35;
+        int BACK_WIDTH = 125;
+        int BACK_HEIGHT = 125;
         if (Gdx.input.getX() < z + BACK_WIDTH && Gdx.input.getX() > z && ScreenSpace.HEIGHT - Gdx.input.getY() < PLAY_Y + BACK_HEIGHT && ScreenSpace.HEIGHT - Gdx.input.getY() > PLAY_Y){
             // If the cursor is around the button, draw the activated button.
-            game.batch.draw(backButtonActivated, z, PLAY_Y, BACK_WIDTH, BACK_WIDTH);
+            game.batch.draw(backButtonActivated, z, PLAY_Y, BACK_WIDTH, BACK_HEIGHT);
             // If the button id clicked, go back to the main menu.
             if (Gdx.input.isTouched()){
                 // If the button is pushed, dispose this screen and go back to the main page.
@@ -127,7 +128,7 @@ public class MazeOption implements Screen {
             }
         } else {
             // If the cursor is not around the button, draw the inactivated button.
-            game.batch.draw(backButtonInactivated, z, PLAY_Y, BACK_WIDTH, BACK_WIDTH);
+            game.batch.draw(backButtonInactivated, z, PLAY_Y, BACK_WIDTH, BACK_HEIGHT);
         }
 
         game.batch.end();
